c7abea4d67fb87456f1d200c611b4636aa70eb2f
Trieu Tien Duc <ductt21@viettel.com.vn>
2025-09-30 17:52:41 +0700

    find one person by food key

--- myApp.js
+++ myApp.js
@@ -63,7 +63,10 @@ const findPeopleByName = (personName, done) => {
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({ favoriteFoods: food }, (err, personFound) => {
+    if (err) return done(err);
+    return done(null, personFound);
+  });
 };
 
 const findPersonById = (personId, done) => {
