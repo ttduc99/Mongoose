85bcaa2bc6612be7c12d71f6ec98d291d13d34cc
Trieu Tien Duc <ductt21@viettel.com.vn>
2025-09-30 19:04:36 +0700

    find by name and update person

--- myApp.js
+++ myApp.js
@@ -91,7 +91,15 @@ const findEditThenSave = (personId, done) => {
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
-
+  Person.findOneAndUpdate(
+    { name: personName },
+    { age: ageToSet },
+    { new: true },
+    (err, updatedPerson) => {
+      if (err) return done(err);
+      return done(null, updatedPerson);
+    }
+  );
   done(null /*, data*/);
 };
 
